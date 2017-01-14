b174eea1dc671363a5b7839c22ca31f764231676
triplus <triplus@users.noreply.github.com>
2017-01-14 19:45:32 +0100

    Verbose tooltip, timer, icons and fixes.
    - Drop run as macro option
    - Simplified indicator start logic
    - Fix index out of range status bar message
    - Tooltip with icons for CAD navigation style
    - Restore arrow to suggest indicator is clickable
    - Use timer to sync the setting (10s) if navigation style is changed elsewhere
    - Set CAD navigation style instead of Undefined if no style is set (first FreeCAD start)
    - Dedicated icon for Undefined, CAD (default), Blender, Touchpad and Gesture navigation styles

--- InitGui.py
+++ InitGui.py
@@ -1,5 +1,5 @@
 # Navigation indicator for FreeCAD
-# Copyright (C) 2016  triplus @ FreeCAD
+# Copyright (C) 2016, 2017  triplus @ FreeCAD
 #
 #
 # This library is free software; you can redistribute it and/or
@@ -16,160 +16,189 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
+"""Navigation indicator for FreeCAD."""
+
 
 def navigationIndicator():
-    """
-    Navigation indicator for FreeCAD.
-    """
+    """Navigation indicator."""
+
     import FreeCAD as App
     import FreeCADGui as Gui
     from PySide import QtGui
     from PySide import QtCore
+    import NavigationIndicatorPath
 
-    icon = """<svg xmlns="http://www.w3.org/2000/svg" height="64" width="64">
-               <path d="m39.075 4.9998c5.2101 0 9.4337 4.2602 9.4337
-                        9.5154v27.826c0 7.8515-7.3914 16.659-16.509
-                        16.659-9.1177
-                        0-16.509-8.7965-16.509-16.645v-27.84c0-5.2552
-                        4.9959-9.5154 9.4459-9.5154h14.138z"
-                        stroke="#2e3436" stroke-width="6" fill="none"/>
-               <path d="m33.886 15.11v13.084a1.8868 1.9032 0 0 1 -3.7735
-                        0v-13.084a1.8868 1.9032 0 1 1 3.7735 0z"
-                        stroke="#2e3436" stroke-width="4" fill="none"/>
-              </svg>"""
+    mw = Gui.getMainWindow()
+    statusBar = mw.statusBar()
+    timer = NavigationIndicatorPath.timer()
+    path = NavigationIndicatorPath.path() + "/Resources/icons/"
+    p = App.ParamGet("User parameter:BaseApp/Preferences/View")
 
-    navIcon = QtGui.QPixmap()
-    navIcon.loadFromData(icon)
+    ttUndefined = "Navigation style not recognized."
 
-    def onStart():
-        """
-        Add navigation indicator to the status bar.
-        """
-        statusBar = mw.statusBar()
-        paramGet = App.ParamGet("User parameter:BaseApp/Preferences/View")
+    ttCAD = str("""
+        <p align='center'><b>CAD</b> navigation style</p>
+        <table>
+            <tr>
+                <th>Select</th>
+                <th>Zoom</th>
+                <th>Rotate</th>
+                <th>Rotate</th>
+                <th>Pan</th>
+            </tr>
+            <tr>
+                <td><img align='center' src='""" + path + """NavigationCAD_Select.svg'></td>
+                <td><img align='center' src='""" + path + """NavigationCAD_Zoom.svg'></td>
+                <td><img align='center' src='""" + path + """NavigationCAD_Rotate.svg'></td>
+                <td><img align='center' src='""" + path + """NavigationCAD_RotateAlt.svg'></td>
+                <td><img align='center' src='""" + path + """NavigationCAD_Pan.svg'></td>
+            </tr>
+        </table>""")
 
-        navStyle = {
-            "OpenInventor": "Gui::InventorNavigationStyle",
-            "CAD": "Gui::CADNavigationStyle",
-            "Blender": "Gui::BlenderNavigationStyle",
-            "MayaGesture": "Gui::MayaGestureNavigationStyle",
-            "Touchpad": "Gui::TouchpadNavigationStyle",
-            "Gesture": "Gui::GestureNavigationStyle",
-            "OpenCascade": "Gui::OpenCascadeNavigationStyle"}
+    style = {
+        "OpenInventor": "Gui::InventorNavigationStyle",
+        "CAD": "Gui::CADNavigationStyle",
+        "Blender": "Gui::BlenderNavigationStyle",
+        "MayaGesture": "Gui::MayaGestureNavigationStyle",
+        "Touchpad": "Gui::TouchpadNavigationStyle",
+        "Gesture": "Gui::GestureNavigationStyle",
+        "OpenCascade": "Gui::OpenCascadeNavigationStyle"}
 
-        navStyleSort = [
-            "OpenInventor",
-            "CAD",
-            "Blender",
-            "MayaGesture",
-            "Touchpad",
-            "Gesture",
-            "OpenCascade"]
+    indicator = QtGui.QToolButton(statusBar)
+    indicator.setAutoRaise(True)
+    indicator.setObjectName("Indicator_Navigation")
+    indicator.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+    indicator.setPopupMode(QtGui.QToolButton
+                           .ToolButtonPopupMode
+                           .InstantPopup)
 
-        def setDefault():
-            """
-            Set default navigation style.
-            """
-            style = False
-            menu.blockSignals(True)
-            defaultStyle = paramGet.GetString("NavigationStyle")
+    menu = QtGui.QMenu(indicator)
+    indicator.setMenu(menu)
 
-            for i in navStyle:
-                if navStyle[i] == defaultStyle:
+    a0 = QtGui.QAction(menu)
+    a0.setIcon(QtGui.QIcon(path + "NavigationUndefined.svg"))
+    a0.setText("Undefined")
+    a0.setData("Undefined")
+    a0.setObjectName("Indicator_NavigationUndefined")
+    a0.setToolTip(ttUndefined)
+
+    a1 = QtGui.QAction(menu)
+    a1.setIcon(QtGui.QIcon(path + "NavigationOpenInventor.svg"))
+    a1.setText("OpenInventor")
+    a1.setObjectName("Indicator_NavigationOpenInventor")
+    a1.setData("Gui::InventorNavigationStyle")
+
+    a2 = QtGui.QAction(menu)
+    a2.setIcon(QtGui.QIcon(path + "NavigationCAD.svg"))
+    a2.setText("CAD")
+    a2.setToolTip(ttCAD)
+    a2.setObjectName("Indicator_NavigationCAD")
+    a2.setData("Gui::CADNavigationStyle")
+
+    a3 = QtGui.QAction(menu)
+    a3.setIcon(QtGui.QIcon(path + "NavigationBlender.svg"))
+    a3.setText("Blender")
+    a3.setObjectName("Indicator_NavigationBlender")
+    a3.setData("Gui::BlenderNavigationStyle")
+
+    a4 = QtGui.QAction(menu)
+    a4.setIcon(QtGui.QIcon(path + "NavigationMayaGesture.svg"))
+    a4.setText("MayaGesture")
+    a4.setObjectName("Indicator_NavigationMayaGesture")
+    a4.setData("Gui::MayaGestureNavigationStyle")
+
+    a5 = QtGui.QAction(menu)
+    a5.setIcon(QtGui.QIcon(path + "NavigationTouchpad.svg"))
+    a5.setText("Touchpad")
+    a5.setObjectName("Indicator_NavigationTouchpad")
+    a5.setData("Gui::TouchpadNavigationStyle")
+
+    a6 = QtGui.QAction(menu)
+    a6.setIcon(QtGui.QIcon(path + "NavigationGesture.svg"))
+    a6.setText("Gesture")
+    a6.setObjectName("Indicator_NavigationGesture")
+    a6.setData("Gui::GestureNavigationStyle")
+
+    a7 = QtGui.QAction(menu)
+    a7.setIcon(QtGui.QIcon(path + "NavigationOpenCascade.svg"))
+    a7.setText("OpenCascade")
+    a7.setObjectName("Indicator_NavigationOpenCascade")
+    a7.setData("Gui::OpenCascadeNavigationStyle")
+
+    menu.addAction(a0)
+    menu.addAction(a1)
+    menu.addAction(a2)
+    menu.addAction(a3)
+    menu.addAction(a4)
+    menu.addAction(a5)
+    menu.addAction(a6)
+    menu.addAction(a7)
+
+    def onMenu(action):
+        """Set navigation style on selection."""
+
+        s = False
+
+        if action.data() != "Undefined":
+            s = True
+            menu.setDefaultAction(action)
+            indicator.setDefaultAction(action)
+            p.SetString("NavigationStyle", action.data())
+        else:
+            pass
+
+        if s:
+            a0.setVisible(False)
+        else:
+            a0.setVisible(True)
+            a0.setEnabled(True)
+            menu.setDefaultAction(a0)
+            indicator.setDefaultAction(a0)
+
+    menu.triggered.connect(onMenu)
+
+    def setCurrent():
+        """Set navigation style on start and on interval."""
+
+        s = False
+        current = p.GetString("NavigationStyle")
+
+        menu.blockSignals(True)
+
+        if current:
+            for i in style:
+                if style[i] == current:
                     for a in menu.actions():
                         if a.text() == i:
-                            style = True
+                            s = True
                             menu.setDefaultAction(a)
                             indicator.setDefaultAction(a)
                         else:
                             pass
                 else:
                     pass
+        else:
+            s = True
+            menu.setDefaultAction(a2)
+            indicator.setDefaultAction(a2)
 
-                if style:
-                    undefined.setVisible(False)
-                else:
-                    undefined.setVisible(True)
-                    undefined.setEnabled(True)
-                    menu.setDefaultAction(undefined)
-                    indicator.setDefaultAction(undefined)
+        if s:
+            a0.setVisible(False)
+        else:
+            a0.setVisible(True)
+            a0.setEnabled(True)
+            menu.setDefaultAction(a0)
+            indicator.setDefaultAction(a0)
 
-            menu.blockSignals(False)
+        menu.blockSignals(False)
 
-        indicator = QtGui.QToolButton(statusBar)
-        indicator.setAutoRaise(True)
-        indicator.setObjectName("NavInd_Button")
-        indicator.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
-        indicator.setStyleSheet("QToolButton::menu-indicator {image: none}")
-        indicator.setPopupMode(QtGui.QToolButton
-                               .ToolButtonPopupMode
-                               .InstantPopup)
+    statusBar.addPermanentWidget(indicator)
+    statusBar.addPermanentWidget(statusBar.children()[2])
 
-        menu = QtGui.QMenu(indicator)
-        indicator.setMenu(menu)
+    setCurrent()
 
-        def onMenu(action):
-            """
-            Set navigation style when an option from the menu is selected.
-            """
-            style = False
+    timer.timeout.connect(setCurrent)
+    timer.start(10000)
 
-            if action.data() != "Undefined":
-                style = True
-                menu.setDefaultAction(action)
-                indicator.setDefaultAction(action)
-                paramGet.SetString("NavigationStyle", action.data())
-            else:
-                pass
-
-            if style:
-                undefined.setVisible(False)
-            else:
-                undefined.setVisible(True)
-                undefined.setEnabled(True)
-                menu.setDefaultAction(undefined)
-                indicator.setDefaultAction(undefined)
-
-        menu.triggered.connect(onMenu)
-
-        for i in navStyleSort:
-            if i in navStyle:
-                action = QtGui.QAction(menu)
-                action.setText(i)
-                action.setIcon(navIcon)
-                action.setData(navStyle[i])
-                action.setObjectName("NavInd_" + i)
-                menu.addAction(action)
-            else:
-                pass
-
-        undefined = QtGui.QAction(menu)
-        undefined.setIcon(navIcon)
-        undefined.setText("Undefined")
-        undefined.setData("Undefined")
-        undefined.setObjectName("NavInd_Undefined")
-        toolTip = "A navigation style set is not recognized by navigation indicator."
-        undefined.setToolTip(toolTip)
-        menu.addAction(undefined)
-
-        setDefault()
-        statusBar.insertPermanentWidget(0, indicator)
-        statusBar.insertPermanentWidget(0, statusBar.children()[2])
-
-    def onMessageChanged():
-        """
-        Start navigation indicator after activity in status bar is detected.
-        """
-        statusBar.messageChanged.disconnect(onMessageChanged)
-        onStart()
-
-    mw = Gui.getMainWindow()
-
-    # Single instance
-    if mw.findChild(QtGui.QToolButton, "NavInd_Button"):
-        pass
-    else:
-        statusBar = mw.statusBar()
-        statusBar.messageChanged.connect(onMessageChanged)
 
 navigationIndicator()
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Navigation indicator for FreeCAD
 
 ### Installation:
 
-NavigationIndicator can be installed as FreeCAD module or it can be used as FreeCAD macro. If NavigationIndicator is installed as FreeCAD module it will be available after FreeCAD is started. Install path for FreeCAD modules depends on the operating system used.
+Install path for FreeCAD modules depends on the operating system used.
 
 ##### Examples:
 
@@ -19,11 +19,6 @@ Windows:
 
 `C:\Users\user_name\AppData\Roaming\FreeCAD\Mod\NavigationIndicator\InitGui.py`
 
-
-##### Note:
-
-When NavigationIndicator is used as macro InitGui.py file can be used directly or for convenience it can be renamed to NavigationIndicator.FCMacro.
-
 ### Usage:
 
-Press on the navigation indicator located in status bar and set desired navigation style.
+Press on the navigation indicator located in the status bar. Set desired navigation style.
