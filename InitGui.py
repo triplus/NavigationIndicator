1fa8d6b37d173394a3de1394ca830e0033348ca5
triplus <triplus@users.noreply.github.com>
2017-02-11 18:57:27 +0100

    Tooltips, settings and orbit style.
    - Orbit style control (Turntable/Trackball)
    - Compact mode (show indicator icon only)
    - Tooltip control (show or hide verbose tooltip information)
    - OpenInventor, Blender, MayaGesture, Touchpad, Gesture and OpenCascade navigation style verbose tooltips

--- InitGui.py
+++ InitGui.py
@@ -32,41 +32,206 @@ def navigationIndicator():
     statusBar = mw.statusBar()
     timer = NavigationIndicatorPath.timer()
     path = NavigationIndicatorPath.path() + "/Resources/icons/"
-    p = App.ParamGet("User parameter:BaseApp/Preferences/View")
+    pView = App.ParamGet("User parameter:BaseApp/Preferences/View")
+    p = App.ParamGet("User parameter:BaseApp/NavigationIndicator")
 
-    ttUndefined = "Navigation style not recognized."
+    t0 = "Navigation style not recognized."
 
-    ttCAD = str("""
-        <p align='center'><b>CAD</b> navigation style</p>
+    t1 = str("""
+        <p align='center'><b>OpenInventor</b> navigation style</p>
         <table>
             <tr>
-                <th>Select</th>
-                <th>Zoom</th>
-                <th>Rotate</th>
-                <th>Rotate</th>
-                <th>Pan</th>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
             </tr>
             <tr>
-                <td><img align='center' src='""" + path + """NavigationCAD_Select.svg'></td>
-                <td><img align='center' src='""" + path + """NavigationCAD_Zoom.svg'></td>
-                <td><img align='center' src='""" + path + """NavigationCAD_Rotate.svg'></td>
-                <td><img align='center' src='""" + path + """NavigationCAD_RotateAlt.svg'></td>
-                <td><img align='center' src='""" + path + """NavigationCAD_Pan.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenInventor_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenInventor_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenInventor_ZoomAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenInventor_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenInventor_Pan.svg'></td>
             </tr>
         </table>""")
 
-    style = {
-        "OpenInventor": "Gui::InventorNavigationStyle",
-        "CAD": "Gui::CADNavigationStyle",
-        "Blender": "Gui::BlenderNavigationStyle",
-        "MayaGesture": "Gui::MayaGestureNavigationStyle",
-        "Touchpad": "Gui::TouchpadNavigationStyle",
-        "Gesture": "Gui::GestureNavigationStyle",
-        "OpenCascade": "Gui::OpenCascadeNavigationStyle"}
+    t2 = str("""
+        <p align='center'><b>CAD</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationCAD_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationCAD_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationCAD_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationCAD_RotateAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationCAD_Pan.svg'></td>
+            </tr>
+        </table>""")
+
+    t3 = str("""
+        <p align='center'><b>Blender</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Pan</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationBlender_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationBlender_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationBlender_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationBlender_Pan.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationBlender_PanAlt.svg'></td>
+            </tr>
+        </table>""")
+
+    t4 = str("""
+        <p align='center'><b>MayaGesture</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Tilt</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_ZoomAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_Pan.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_Tilt.svg'></td>
+            </tr>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Tilt</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_SelectTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_ZoomTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_RotateTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_PanTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_PanTouchAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationMayaGesture_TiltTouch.svg'></td>
+            </tr>
+        </table>
+        <p><small><b>Zoom:</b> Page Up or Page Down key.<br>
+        <b>Rotation focus:</b> Middle mouse button or key H.</small></p>""")
+
+    t5 = str("""
+        <p align='center'><b>Touchpad</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_ZoomAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_RotateAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_Pan.svg'></td>
+            </tr>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_SelectTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_ZoomTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_RotateTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_RotateTouchAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationTouchpad_PanTouch.svg'></td>
+            </tr>
+        </table>
+        <p><small><b>Zoom:</b> Page Up or Page Down key.</small></p>""")
+
+    t6 = str("""
+        <p align='center'><b>Gesture</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Tilt</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationGesture_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_RotateAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_Pan.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_Tilt.svg'></td>
+            </tr>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Tilt</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationGesture_SelectTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_ZoomTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_RotateTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_PanTouch.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_PanTouchAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationGesture_TiltTouch.svg'></td>
+            </tr>
+        </table>
+        <p><small><b>Zoom:</b> Page Up or Page Down key.<br>
+        <b>Rotation focus:</b> Middle mouse button or key H.</small></p>""")
+
+    t7 = str("""
+        <p align='center'><b>OpenCascade</b> navigation style</p>
+        <table>
+            <tr>
+                <th><small>Select</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Zoom</small></th>
+                <th><small>Rotate</small></th>
+                <th><small>Pan</small></th>
+                <th><small>Pan</small></th>
+            </tr>
+            <tr>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_Select.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_Zoom.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_ZoomAlt.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_Rotate.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_Pan.svg'></td>
+                <td align='center'><img src='""" + path + """NavigationOpenCascade_PanAlt.svg'></td>
+            </tr>
+        </table>""")
 
     indicator = QtGui.QToolButton(statusBar)
     indicator.setAutoRaise(True)
-    indicator.setObjectName("Indicator_Navigation")
+    indicator.setObjectName("Std_NavigationIndicator")
     indicator.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
     indicator.setPopupMode(QtGui.QToolButton
                            .ToolButtonPopupMode
@@ -75,56 +240,85 @@ def navigationIndicator():
     menu = QtGui.QMenu(indicator)
     indicator.setMenu(menu)
 
-    a0 = QtGui.QAction(menu)
+    menuSettings = QtGui.QMenu("Settings", menu)
+    menuOrbit = QtGui.QMenu("Orbit style", menu)
+
+    aCompact = QtGui.QAction(menuSettings)
+    aCompact.setText("Compact")
+    aCompact.setCheckable(True)
+    aTooltip = QtGui.QAction(menuSettings)
+    aTooltip.setText("Tooltip")
+    aTooltip.setCheckable(True)
+
+    gOrbit = QtGui.QActionGroup(menuSettings)
+
+    aTurntable = QtGui.QAction(gOrbit)
+    aTurntable.setText("Turntable")
+    aTurntable.setCheckable(True)
+    aTrackball = QtGui.QAction(gOrbit)
+    aTrackball.setText("Trackball")
+    aTrackball.setCheckable(True)
+
+    menuOrbit.addAction(aTurntable)
+    menuOrbit.addAction(aTrackball)
+
+    menuSettings.addMenu(menuOrbit)
+    menuSettings.addSeparator()
+    menuSettings.addAction(aCompact)
+    menuSettings.addAction(aTooltip)
+
+    gStyle = QtGui.QActionGroup(menu)
+
+    a0 = QtGui.QAction(gStyle)
     a0.setIcon(QtGui.QIcon(path + "NavigationUndefined.svg"))
     a0.setText("Undefined")
     a0.setData("Undefined")
     a0.setObjectName("Indicator_NavigationUndefined")
-    a0.setToolTip(ttUndefined)
 
-    a1 = QtGui.QAction(menu)
+    a1 = QtGui.QAction(gStyle)
     a1.setIcon(QtGui.QIcon(path + "NavigationOpenInventor.svg"))
     a1.setText("OpenInventor")
     a1.setObjectName("Indicator_NavigationOpenInventor")
     a1.setData("Gui::InventorNavigationStyle")
 
-    a2 = QtGui.QAction(menu)
+    a2 = QtGui.QAction(gStyle)
     a2.setIcon(QtGui.QIcon(path + "NavigationCAD.svg"))
     a2.setText("CAD")
-    a2.setToolTip(ttCAD)
     a2.setObjectName("Indicator_NavigationCAD")
     a2.setData("Gui::CADNavigationStyle")
 
-    a3 = QtGui.QAction(menu)
+    a3 = QtGui.QAction(gStyle)
     a3.setIcon(QtGui.QIcon(path + "NavigationBlender.svg"))
     a3.setText("Blender")
     a3.setObjectName("Indicator_NavigationBlender")
     a3.setData("Gui::BlenderNavigationStyle")
 
-    a4 = QtGui.QAction(menu)
+    a4 = QtGui.QAction(gStyle)
     a4.setIcon(QtGui.QIcon(path + "NavigationMayaGesture.svg"))
     a4.setText("MayaGesture")
     a4.setObjectName("Indicator_NavigationMayaGesture")
     a4.setData("Gui::MayaGestureNavigationStyle")
 
-    a5 = QtGui.QAction(menu)
+    a5 = QtGui.QAction(gStyle)
     a5.setIcon(QtGui.QIcon(path + "NavigationTouchpad.svg"))
     a5.setText("Touchpad")
     a5.setObjectName("Indicator_NavigationTouchpad")
     a5.setData("Gui::TouchpadNavigationStyle")
 
-    a6 = QtGui.QAction(menu)
+    a6 = QtGui.QAction(gStyle)
     a6.setIcon(QtGui.QIcon(path + "NavigationGesture.svg"))
     a6.setText("Gesture")
     a6.setObjectName("Indicator_NavigationGesture")
     a6.setData("Gui::GestureNavigationStyle")
 
-    a7 = QtGui.QAction(menu)
+    a7 = QtGui.QAction(gStyle)
     a7.setIcon(QtGui.QIcon(path + "NavigationOpenCascade.svg"))
     a7.setText("OpenCascade")
     a7.setObjectName("Indicator_NavigationOpenCascade")
     a7.setData("Gui::OpenCascadeNavigationStyle")
 
+    menu.addMenu(menuSettings)
+    menu.addSeparator()
     menu.addAction(a0)
     menu.addAction(a1)
     menu.addAction(a2)
@@ -133,6 +327,41 @@ def navigationIndicator():
     menu.addAction(a5)
     menu.addAction(a6)
     menu.addAction(a7)
+
+    def onCompact():
+        """Enable or disable compact mode."""
+        if aCompact.isChecked():
+            indicator.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+            indicator.setStyleSheet("QToolButton::menu-indicator {image: none}")
+            p.SetBool("Compact", 1)
+        else:
+            indicator.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+            indicator.setStyleSheet("QToolButton::menu-indicator {}")
+            p.SetBool("Compact", 0)
+
+    def onTooltip():
+        """Enable or disable verbose tooltips."""
+        if aTooltip.isChecked():
+            a0.setToolTip(t0)
+            a1.setToolTip(t1)
+            a2.setToolTip(t2)
+            a3.setToolTip(t3)
+            a4.setToolTip(t4)
+            a5.setToolTip(t5)
+            a6.setToolTip(t6)
+            a7.setToolTip(t7)
+            p.SetBool("Tooltip", 1)
+        else:
+            for i in gStyle.actions():
+                i.setToolTip("")
+            p.SetBool("Tooltip", 0)
+
+    def onOrbit():
+        """Use turntable or trackball orbit style."""
+        if aTurntable.isChecked():
+            pView.SetInt("OrbitStyle", 1)
+        else:
+            pView.SetInt("OrbitStyle", 0)
 
     def onMenu(action):
         """Set navigation style on selection."""
@@ -143,7 +372,7 @@ def navigationIndicator():
             s = True
             menu.setDefaultAction(action)
             indicator.setDefaultAction(action)
-            p.SetString("NavigationStyle", action.data())
+            pView.SetString("NavigationStyle", action.data())
         else:
             pass
 
@@ -155,32 +384,28 @@ def navigationIndicator():
             menu.setDefaultAction(a0)
             indicator.setDefaultAction(a0)
 
-    menu.triggered.connect(onMenu)
-
     def setCurrent():
         """Set navigation style on start and on interval."""
 
-        s = False
-        current = p.GetString("NavigationStyle")
+        gStyle.blockSignals(True)
 
-        menu.blockSignals(True)
+        s = False
+        actions = gStyle.actions()
+        current = pView.GetString("NavigationStyle")
 
         if current:
-            for i in style:
-                if style[i] == current:
-                    for a in menu.actions():
-                        if a.text() == i:
-                            s = True
-                            menu.setDefaultAction(a)
-                            indicator.setDefaultAction(a)
-                        else:
-                            pass
+            for i in actions:
+                if i.data() == current:
+                    s = True
+                    menu.setDefaultAction(i)
+                    indicator.setDefaultAction(i)
                 else:
                     pass
         else:
             s = True
             menu.setDefaultAction(a2)
             indicator.setDefaultAction(a2)
+            pView.SetString("NavigationStyle", a2.data())
 
         if s:
             a0.setVisible(False)
@@ -190,12 +415,32 @@ def navigationIndicator():
             menu.setDefaultAction(a0)
             indicator.setDefaultAction(a0)
 
-        menu.blockSignals(False)
+        gStyle.blockSignals(False)
+
+    if p.GetBool("Compact"):
+        aCompact.setChecked(True)
+
+    if p.GetBool("Tooltip", 1):
+        aTooltip.setChecked(True)
+
+    if pView.GetInt("OrbitStyle", 1) == 0:
+        aTrackball.setChecked(True)
+    else:
+        aTurntable.setChecked(True)
+
+    onCompact()
+    onTooltip()
+    onOrbit()
 
     statusBar.addPermanentWidget(indicator)
     statusBar.addPermanentWidget(statusBar.children()[2])
 
     setCurrent()
+
+    gStyle.triggered.connect(onMenu)
+    gOrbit.triggered.connect(onOrbit)
+    aCompact.triggered.connect(onCompact)
+    aTooltip.triggered.connect(onTooltip)
 
     timer.timeout.connect(setCurrent)
     timer.start(10000)
